21b544de8229ce75e169d6285ccbb3de28e417da
Ruihan Chen <rvc5509@psu.edu>
2023-02-02 14:24:04 -0500

    generate specialization data

--- db_scripts.py
+++ db_scripts.py
@@ -105,34 +105,35 @@ def setup_CourseToSpec_table(file):
     # Create a cursor object to execute SQL commands
     cursor = conn.cursor()
 
-    # Create Bin table
+    cursor.execute("""DROP TABLE IF EXISTS COURSE_TO_SPEC""")    # Create Bin table
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS COURSE_TO_SPEC (
-            subject INTEGER,
+            subject TEXT,
             number TEXT,
+            name TEXT,
             spec_id INTEGER,
-            PRIMARY KEY (subject, number, spec_id)
+            PRIMARY KEY (subject, number, name, spec_id)
         )
     """)
 
-    # # import Bin data
-    # with open("csv/spec.csv", "r") as file:
-    #     reader = csv.reader(file)
+    # import COURSE_TO_SPEC data
+    with open("specialization data/data_with_name.csv", "r") as file:
+        reader = csv.reader(file)
         
-    #     for row in reader:
-    #         print(row)
-    #         cursor.execute("""
-    #             INSERT INTO SPECIALIZATION (spec_id, spec_name)
-    #             VALUES (?, ?)
-    #         """, row)
-    # Commit the changes and close the connection
+        for row in reader:
+            print(row)
+            cursor.execute("""
+                INSERT OR IGNORE INTO COURSE_TO_SPEC (subject, number, name, spec_id)
+                VALUES (?, ?, ?, ?)
+            """, row)
+
     conn.commit()
     conn.close()
 
 
 if __name__ == "__main__":
     file = "graduate_tracking_system.db"
-    setup_courses_table(file)
-    setup_bin_table(file)
-    setup_Specializations_table(file)
-    # setup_CourseToSpec_table(file)
+    # setup_courses_table(file)
+    # setup_bin_table(file)
+    # setup_Specializations_table(file)
+    setup_CourseToSpec_table(file)
