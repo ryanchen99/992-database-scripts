896efbb08aabcf3cdded32e28830705884472fa7
Ruihan Chen <rvc5509@psu.edu>
2023-02-01 20:30:39 -0500

    remove space after comma

--- data_processing.py
+++ data_processing.py
@@ -3,9 +3,9 @@ Lines = file.readlines()
 file2 = open("csv/data.txt", "w")
 for line in Lines:
     if line[:4] == "INLS":
-        writeLine = "INLS, "+line[5:8]+", "
+        writeLine = "INLS,"+line[5:8]+",data"
         pare_idx = line.find("(")
-        writeLine += line[10:pare_idx-1] + ", "
+        writeLine += line[10:pare_idx-1] + ","
         credits = "3"
         if line[pare_idx+1] == "1":
             credits = "1.5"
